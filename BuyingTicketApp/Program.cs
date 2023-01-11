fcaba4136cc57e0c50d4ca213e333591eb3dfb8e
alvinwendt <alvinwendt@gmail.com>
2023-01-10 21:00:45 -0600

    Add project files.

---
+++ BuyingTicketApp/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace BuyingTicketApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int tickets = 0;
+            string input = "";
+
+            while (tickets <=0)
+            {
+                try
+                {
+                    Console.WriteLine("How many tickets would you like to purchase?");
+                    input = Console.ReadLine();
+                    tickets = Int32.Parse(input);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter a valid number");
+
+                }
+
+            }
+
+            Console.Clear();
+
+            if (tickets == 1)
+            {
+             Console.WriteLine($"You have just purchased {input} ticket!");
+            }
+            else
+            {
+                Console.WriteLine($"You have just purchased {input} tickets!");
+            }
+            
+            
+        }
+    }
+}
